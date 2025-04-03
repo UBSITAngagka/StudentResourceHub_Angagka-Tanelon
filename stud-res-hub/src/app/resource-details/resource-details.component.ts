4f619ab2097e4e2aa29a021de5113207bb9f9245
UBSIT Tanelon <20183150@s.ubaguio.edu>
2025-04-03 21:50:07 +0800

    Added Resource Details

--- stud-res-hub/src/app/resource-details/resource-details.component.ts
+++ stud-res-hub/src/app/resource-details/resource-details.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-resource-details',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './resource-details.component.html',
   styleUrl: './resource-details.component.css'
 })
 export class ResourceDetailsComponent {
-
+  isDisabled = true;
 }
